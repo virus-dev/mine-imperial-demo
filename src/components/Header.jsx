05fe631bb2ecbc5367c8f8d0f27b269186da05d9
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-08-04 12:10:31 +0300

    Create Header components
    - Add sass-loader
    - Add scss folder
    - Add fonts

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import burgerMenu from '../img/burgerMenu.svg'
+
+function Header() {
+    const [burgerMenuOpened, setBurgerMenuOpened] = useState(false);
+    const toggleOpenBurgerMenu = () => {
+        setBurgerMenuOpened(!burgerMenuOpened);
+    }
+    const burgerIcon = React.useRef();
+
+    const handleOutsideClick = (e) => {
+        if (!e.path.includes(burgerIcon.current)) {
+            setBurgerMenuOpened(false)
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick)
+    }, []);
+
+    return (
+        <header>
+            <div className="nothingLine"></div>
+            <div className="container">
+                <div className="header__inner">
+                    <div className="header__logo header__item">Лого</div>
+                    <div className="header__infoPage header__item">Главная страница</div>
+                    <div className="header__user header__item">Гость</div>
+                    <div ref={burgerIcon} className="header__burger header__item">
+                        <img onClick={() => toggleOpenBurgerMenu()} src={burgerMenu} alt="Бургер меню"/>
+                        {burgerMenuOpened && 
+                        <div className="header__burgerMenu">
+                            <div className="header__burgerMenu__item">Карта ада</div>
+                        </div>}
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
