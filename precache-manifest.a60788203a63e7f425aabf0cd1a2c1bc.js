73c62f6da101c1f7a37dad3edb8d92fa0d029ea1
Nikita <nikita-pashin12@yandex.ru>
2020-10-27 12:20:35 +0500

    Updates

---
+++ precache-manifest.a60788203a63e7f425aabf0cd1a2c1bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33652078fb1c7f5f0e810bb78c261ba7",
+    "url": "/mine-imperial-demo/index.html"
+  },
+  {
+    "revision": "d198e878f273794706c2",
+    "url": "/mine-imperial-demo/static/css/main.a5367f2d.chunk.css"
+  },
+  {
+    "revision": "d91f9fa49424ef95c1c9",
+    "url": "/mine-imperial-demo/static/js/2.5d3886f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mine-imperial-demo/static/js/2.5d3886f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d198e878f273794706c2",
+    "url": "/mine-imperial-demo/static/js/main.2231edb1.chunk.js"
+  },
+  {
+    "revision": "64196ba54fc0040c329e",
+    "url": "/mine-imperial-demo/static/js/runtime-main.32c12801.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/mine-imperial-demo/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/mine-imperial-demo/static/media/Roboto-BlackItalic.99682a78.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/mine-imperial-demo/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/mine-imperial-demo/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/mine-imperial-demo/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/mine-imperial-demo/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/mine-imperial-demo/static/media/burgerMenu.86595150.svg"
+  },
+  {
+    "revision": "ce792445a7c98ff7f2d57e8aec0ff3ab",
+    "url": "/mine-imperial-demo/static/media/mainPage.ce792445.jpg"
+  }
+]);
