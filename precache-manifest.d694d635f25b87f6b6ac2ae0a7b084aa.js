b9f7dd78c935e7190cf857506000768c6b2d5695
Nikita <nikita-pashin12@yandex.ru>
2020-12-07 22:08:31 +0500

    Updates

--- precache-manifest.d694d635f25b87f6b6ac2ae0a7b084aa.js
+++ precache-manifest.d694d635f25b87f6b6ac2ae0a7b084aa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0281c353a2434f4db1b351d4141466fe",
+    "revision": "0ba12613c549a3696cc2188866abbbe4",
     "url": "/mine-imperial-demo/index.html"
   },
   {
-    "revision": "36d58c0652bfcf0e2cae",
+    "revision": "7771014186f05ac1aacf",
     "url": "/mine-imperial-demo/static/css/main.a5367f2d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mine-imperial-demo/static/js/2.23eb2632.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "36d58c0652bfcf0e2cae",
-    "url": "/mine-imperial-demo/static/js/main.4691c94d.chunk.js"
+    "revision": "7771014186f05ac1aacf",
+    "url": "/mine-imperial-demo/static/js/main.bc6895aa.chunk.js"
   },
   {
     "revision": "64196ba54fc0040c329e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mine-imperial-demo/precache-manifest.2bd69a2ea6335d7a647461d8fd417fd8.js"
+  "/mine-imperial-demo/precache-manifest.d694d635f25b87f6b6ac2ae0a7b084aa.js"
 );
 
 self.addEventListener('message', (event) => {
